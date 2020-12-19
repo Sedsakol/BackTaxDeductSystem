690b1e8aa3138e3ab7b9b478dadf6aa96427d3e7
Sedsakol Kamolchote <posezaza@gmail.com>
2020-12-19 09:16:29 +0700

    re working tree

--- BackTaxDeductSystem/settings.py
+++ BackTaxDeductSystem/settings.py
@@ -126,8 +126,8 @@ DATABASES = {
         'NAME': 'taxdeduct',
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST': os.environ.get("HOST_DB", default='127.0.0.1'),
-        'PORT': os.environ.get("PORT_DB", default='3308')
+        'HOST': os.environ.get("HOST_DB", default='161.246.5.140'),
+        'PORT': os.environ.get("PORT_DB", default='80')
     }
 }
 
