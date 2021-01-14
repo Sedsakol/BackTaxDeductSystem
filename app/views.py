7e761871445afbb3c6a322d027bdd28258459803
Sedsakol <sedsakol.ka@gmail.com>
2021-01-15 01:07:43 +0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -6655,6 +6655,7 @@ class categories(View):
             content_id = int(content["id"])
             content_data = content["likes"]
             self.create_facebook_categories(content_id = content_id, content_data = content_data)
+            print('created facebook categories for '+str(content_id) +' complete')
 
 
         #debug for heroku
