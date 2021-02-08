25a294a9f2a947a09b2111eb691ee23a4baba86f
Sedsakol <sedsakol.ka@gmail.com>
2021-02-08 16:06:07 +0700

    update ip

--- BackTaxDeductSystem/settings.py
+++ BackTaxDeductSystem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'w1*eww-u*ua2m3u$wak6w-oe5&ks($z+&w@twe-&7j3rum)!ne'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','1.4.173.221','[::1]','taxdeduct.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','101.109.214.180','[::1]','taxdeduct.herokuapp.com']
 
 
 # Application definition
@@ -116,7 +116,7 @@ DATABASES = {
         'NAME': 'taxdeduct',
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST': os.environ.get("HOST_DB", default='1.4.173.221'),
+        'HOST': os.environ.get("HOST_DB", default='101.109.214.180'),
         'PORT': os.environ.get("PORT_DB", default='3306'),
         'OPTIONS': {'charset': 'utf8mb4'},
     }
